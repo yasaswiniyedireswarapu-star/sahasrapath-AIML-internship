377eea7e59a20c0f3a99581a7239f5205510ed95
yasaswiniyedireswarapu-star <24B11DS231@adityauniversity.in>
2026-05-11 21:47:44 +0530

    task3.py

--- Basic-programs-of-python/task3.py
+++ Basic-programs-of-python/task3.py
@@ -1,14 +1,3 @@
-#to print numbers 1 to 10
-for i in range(1,11):
-  print(i)
-output:
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
+n = int(input("Enter a number: "))
+for i in range(1, n + 1):
+    print(i, end=" ")
