cf2ab8bd308c47508459dee87967b4fa602ca3af
yasaswiniyedireswarapu-star <24B11DS231@adityauniversity.in>
2026-05-11 21:49:53 +0530

    task4.py

--- Basic-programs-of-python/task4.py
+++ Basic-programs-of-python/task4.py
@@ -1,15 +1,4 @@
-#To print Even numbers from 1 to 20
-for i in range(1,21):
-    if i % 2 ==0:
-        print(i)
-output:
-2
-4
-6
-8
-10
-12
-14
-16
-18
-20
+n = int(input("Enter a number: "))
+for i in range(1, n + 1):
+    if i % 2 == 0:
+        print(i, end=" ")
