55ebb365fbf27a8b4e6daca812e005309ed9feec
yasaswiniyedireswarapu-star <24B11DS231@adityauniversity.in>
2026-05-11 20:51:44 +0530

    add_two_numbers.py

---
+++ Basic-programs-of-python/add_two_numbers.py
@@ -0,0 +1,9 @@
+#print add two numbers 
+n1=int(input("Enter first number:"))
+n2=int(input("Enter second number:"))
+sum=n1+n2
+print("addition:",sum)
+output:
+Enter first number: 5
+Enter second number: 10
+addition: 15
