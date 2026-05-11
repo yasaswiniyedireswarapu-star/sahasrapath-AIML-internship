7beb0c9c3e70ece2ce02f34296d1763592197259
yasaswiniyedireswarapu-star <24B11DS231@adityauniversity.in>
2026-05-11 21:48:43 +0530

    task2.py

--- Basic-programs-of-python/task2.py
+++ Basic-programs-of-python/task2.py
@@ -1,9 +1,4 @@
-#print add two numbers 
 n1=int(input("Enter first number:"))
 n2=int(input("Enter second number:"))
 sum=n1+n2
 print("addition:",sum)
-output:
-Enter first number: 5
-Enter second number: 10
-addition: 15
