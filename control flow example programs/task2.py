9087b9deda9df9a61a0497fd0f429743c141494c
yasaswiniyedireswarapu-star <24B11DS231@adityauniversity.in>
2026-05-14 18:17:23 +0530

    task2.py

---
+++ control flow example programs/task2.py
@@ -0,0 +1,6 @@
+username = input("Enter username: ")
+password = input("Enter password: ")
+if username == "admin" and password == "1234":
+    print("Login Successful")
+else:
+    print("Invalid Credentials")
