862ee6e9d09d5c4d12e1e2a8683a82e4b158bdc7
yasaswiniyedireswarapu-star <24B11DS231@adityauniversity.in>
2026-05-14 18:16:38 +0530

    task1.py

---
+++ control flow example programs/task1.py
@@ -0,0 +1,11 @@
+marks = int(input("Enter student marks: "))
+if marks >= 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+elif marks >= 60:
+    print("Grade C")
+elif marks >= 40:
+    print("Grade D")
+else:
+    print("Fail")
